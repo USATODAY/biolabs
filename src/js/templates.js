c49a8e17c421a7bba4519d886ae8690ab0f64493
maddy <mdeason89@gmail.com>
2015-03-17 17:30:13 -0400

    style changes

--- src/js/templates.js
+++ src/js/templates.js
@@ -16,7 +16,7 @@ this["templates"]["intro.html"] = function(obj) {
 obj || (obj = {});
 var __t, __p = '', __e = _.escape;
 with (obj) {
-__p += '<div class="page-wrap index">\n  <h1 class="page-header container"> \n    <img class="info" src="/img/info.svg" alt="info">\n    <img class="logo" src="/img/biologo.svg" alt="BIOLABS IN YOUR BACKYARD">\n   </h1>\n\n  <p class="text"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p>\n\n  <div class="button intro-view-state-button">view your state</div>\n  <div class="button intro-read-story-button">read story</div>\n\n  <div class ="container foot"> \n    <div class="ftbutton"> share </div>\n  </div>\n</div>\n\n';
+__p += '<div class="page-wrap index">\n  <h1 class="page-header container"> \n    <img class="info" src="/img/info.svg" alt="info">\n    <img class="logo" src="/img/biologo.svg" alt="BIOLABS IN YOUR BACKYARD">\n   </h1>\n\n  <p class="text intro"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p>\n\n  <div class="button intro-view-state-button">view your state</div>\n  <div class="button intro-read-story-button">read story</div>\n\n  <div class ="container foot"> \n    <div class="ftbutton"> share </div>\n  </div>\n</div>\n\n';
 
 }
 return __p
@@ -26,21 +26,23 @@ this["templates"]["labView.html"] = function(obj) {
 obj || (obj = {});
 var __t, __p = '', __e = _.escape;
 with (obj) {
-__p += '<div class="page-wrap content">\n  <h1 class="page-header container"> \n    <img class="info" src="/img/info.svg" alt="info">\n    <img class="headimg vt" src="/img/virginiatech.svg" alt="Virginia">\n  </h1>\n\n  <img class="photo" src="/img/vtphoto.svg">\n  <figcaption class="photocred">(Photo: ' +
-((__t = ( photo_credit )) == null ? '' : __t) +
-')</figcaption>\n\n  <a class="link" href="https://www.ento.vt.edu/facilities/bsl-labs.html" target="_blank">\n    <p class="text labnm">' +
+__p += '<div class="page-wrap content">\n  <div class="main">\n    <h1 class="page-header container"> \n      <img class="info" src="/img/info.svg" alt="info">\n      <img class="headimg" src="/img/locator.svg" alt="">\n      <div class="text header labhd">' +
 ((__t = ( name )) == null ? '' : __t) +
-'</p>\n  </a>\n  <p class="text chatt">' +
+'</div>\n    </h1>\n\n    <img class="photo" src="/img/vtphoto.svg">\n    <figcaption class="photocred">(Photo: ' +
+((__t = ( photo_credit )) == null ? '' : __t) +
+')</figcaption>\n\n    <a class="link" href="" target="_blank">\n      <p class="text labnm">' +
+((__t = ( name )) == null ? '' : __t) +
+'</p>\n    </a>\n    <p class="text chatt">' +
 ((__t = ( city )) == null ? '' : __t) +
 ', ' +
 ((__t = ( state )) == null ? '' : __t) +
-'</p>\n  <div class="hr1"></div>\n  <p class="text chatt lab-body">' +
+'</p>\n    <div class="hr1"></div>\n    <p class="text chatt lab-body">' +
 ((__t = ( description )) == null ? '' : __t) +
-'</p>\n\n\n  <div class ="container foot">\n    <p class="byline"> by ' +
+'</p>\n  </div>\n\n  <div class ="container foot">\n    <p class="byline"> by ' +
 ((__t = ( byline )) == null ? '' : __t) +
-'</p> \n      <div class="document-button">\n          download incident documentation          \n      </div>\n    <div class="ftbutton">read story</div>\n    <div class="backtostate">\n      <div class="ftbutton">' +
+'</p> \n      <div class="document-button">\n          download incident documentation          \n      </div>\n    <div class="ftbutton">read story</div>\n    <div class="ftbutton backtostate">' +
 ((__t = ( state )) == null ? '' : __t) +
-'</div>\n    </div>\n    <div class="ftbutton">share</div>\n  </div>\n</div>';
+'</div>\n    <div class="ftbutton">share</div>\n  </div>\n</div>';
 
 }
 return __p
@@ -66,7 +68,7 @@ this["templates"]["stateIndexView.html"] = function(obj) {
 obj || (obj = {});
 var __t, __p = '', __e = _.escape;
 with (obj) {
-__p += '<div class="page-wrap content">\n<h1 class="page-header container"> \n    <img class="info" src="/img/info.svg" alt="info">\n    <img class="headimg" src="/img/states.svg" alt="States">\n  </h1>\n\n  <div class="stcont">\n  </div>\n\n<img class="range" src="/img/range.svg">\n \n\n  <div class ="container foot"> \n    <div class="ftbutton">read story</div>\n    <a href="index.html">\n      <div class="ftbutton">intro</div>\n    </a>\n    <div class="ftbutton">share</div>\n  </div>\n  </div>\n';
+__p += '<div class="page-wrap content">\n  <div class="main">\n    <h1 class="page-header container"> \n      <img class="info" src="/img/info.svg" alt="info">\n      <img class="headimg" src="/img/locator.svg" alt="">\n      <div class="text header"> States </div>\n    </h1>\n    <div class="stcont"></div>\n    <img class="range" src="/img/range.svg">\n  </div>\n\n  <div class ="container foot"> \n    <div class="ftbutton">read story</div>\n    <div class="ftbutton backtointro">intro</div>\n    <div class="ftbutton">share</div>\n  </div>\n</div>\n';
 
 }
 return __p
@@ -76,7 +78,9 @@ this["templates"]["stateView.html"] = function(obj) {
 obj || (obj = {});
 var __t, __p = '', __e = _.escape;
 with (obj) {
-__p += '<div class="page-wrap content">\n  <h1 class="page-header container"> \n    <img class="info" src="/img/info.svg" alt="info">\n    <img class="headimg" src="/img/virginia.svg" alt="Virginia">\n  </h1>\n\n  <p class="text chatt">  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.</p>\n  <div class="hr1"></div>\n\n<div class="lablistcont"></div>\n\n  <div class ="container foot"> \n    <div class="ftbutton">read story</div>\n    <div class="backtoindex">\n      <div class="ftbutton">States</div>\n    </div>\n    <div class="ftbutton">share</div>\n  </div>\n</div>';
+__p += '<div class="page-wrap content">\n\n  <div class="main">\n    <h1 class="page-header container"> \n      <img class="info" src="/img/info.svg" alt="info">\n      <img class="headimg" src="/img/locator.svg" alt="">\n      <div class="text header">' +
+((__t = ( state )) == null ? '' : __t) +
+'</div>\n    </h1>\n    <div class="hr1"></div>\n    <div class="lablistcont"></div>\n  </div>\n\n  <div class ="container foot"> \n    <div class="ftbutton">read story</div>\n    <div class="ftbutton backtoindex">States</div>\n    <div class="ftbutton">share</div>\n  </div>\n\n</div>';
 
 }
 return __p
--- src/js/views/stateView.js
+++ src/js/views/stateView.js
@@ -18,8 +18,9 @@ define(
         template: templates['stateView.html'],
         render: function(data) {
             console.log(this.collection.toJSON());
+            var name = this.collection.models[0].get("state");
             
-            this.$el.html(this.template({states: this.collection.toJSON()}));
+            this.$el.html(this.template({states: this.collection.toJSON(), state: name}));
             this.collection.each(function(labmodel) {
                 var entryView = new stateEntryView({
                     model: labmodel
